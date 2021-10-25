2c27c1b6d41a138289601b3241f95846475dcedd
José Angel Azucena <angelvanitas555@gmail.com>
2021-10-25 16:13:31 -0600

    editando clase uno

--- dia_uno/clase_uno.py
+++ dia_uno/clase_uno.py
@@ -1,4 +1,21 @@
-print("hola mundo en python")
+#programa para uso de variables
+"""Esto es un 
+comentario multilinea"""
+
+saludo = "Hola desde este programa"
+print(saludo)
+print(type(saludo))
+
+edad = 20
+print("La edad es:", edad)
+print("El tipo de dato de edad es: ", type(edad))
+
+precio = 60000.34
+print(precio)
+print("El tipo de dato de precio es:", type(precio))
+
+es_blanco = True
+print("la variable blanco es:", es_blanco)
 
 
 
