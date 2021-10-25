6f66fc5aec1418d1a1f683532b7339c6f5bcc2c2
José Angel Azucena <angelvanitas555@gmail.com>
2021-10-25 16:35:04 -0600

    operaciones aritmeticas

--- dia_uno/clase_uno.py
+++ dia_uno/clase_uno.py
@@ -17,5 +17,20 @@ print("El tipo de dato de precio es:", type(precio))
 es_blanco = True
 print("la variable blanco es:", es_blanco)
 
+#programa para operaciones aritmeticas
+a = 10
+b = 3
+
+print("La suma es:", str(a+b))
+print("La resta es:", a-b)
+
+c = "20"
+
+print(a*b+int(c))
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
 
 
